0d4efadfdb7ccc6c6d9ef37377a051b3fc5c25bc
devide <devide@126.com>
2021-06-25 17:36:00 +0800

    1

---
+++ MESWebApi/InterFaces/IOperateData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESWebApi.InterFaces
+{
+    public interface IOperateData<T> where T : class, new()
+    {
+        int Add(T entity);
+        int Add(List<T> entitys);
+        int Modify(T entity);
+        int Delete(T entity);
+        int Delete(List<T> entitys);
+    }
+}
